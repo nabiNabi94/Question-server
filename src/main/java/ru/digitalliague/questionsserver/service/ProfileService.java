55bdd2f00fb7329543b4d7643d8ba12b1afe7e91
Vladislav Nabiev <v.nabi@icloud.com>
2022-03-09 15:49:35 +0300

    Add output to the getAllProfile method of the ProfileService

--- src/main/java/ru/digitalliague/questionsserver/service/ProfileService.java
+++ src/main/java/ru/digitalliague/questionsserver/service/ProfileService.java
@@ -19,6 +19,7 @@ public class ProfileService {
        public List<ProfileDto> getAllProfile(){
            List<Profile> all = repository.findAll();
            if (all.isEmpty()) throw new ProfileNotFoundException();
+           System.out.println("sd");
           return mapper.mapping(all,ProfileDto.class);
        }
 
