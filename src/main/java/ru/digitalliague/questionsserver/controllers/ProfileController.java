cd99259670efc83f13e089581cbb09f1960492c3
Vladislav Nabiev <v.nabi@icloud.com>
2022-03-10 10:35:02 +0300

    DI ProfileService to the ProfileController

--- src/main/java/ru/digitalliague/questionsserver/controllers/ProfileController.java
+++ src/main/java/ru/digitalliague/questionsserver/controllers/ProfileController.java
@@ -2,10 +2,13 @@ package ru.digitalliague.questionsserver.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.digitalliague.questionsserver.service.ProfileService;
 
 @RestController
 @RequestMapping("/profile")
 public class ProfileController {
+
+    private ProfileService profileService;
 
 
 }
