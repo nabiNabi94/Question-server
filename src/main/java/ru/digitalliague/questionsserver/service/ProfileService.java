c98b40bdf42f513f5feac2952a0bbee27d4c3161
Vladislav Nabiev <v.nabi@icloud.com>
2022-03-10 11:33:57 +0300

    new output

--- src/main/java/ru/digitalliague/questionsserver/service/ProfileService.java
+++ src/main/java/ru/digitalliague/questionsserver/service/ProfileService.java
@@ -19,6 +19,7 @@ public class ProfileService {
        public List<ProfileDto> getAllProfile(){
            List<Profile> all = repository.findAll();
            if (all.isEmpty()) throw new ProfileNotFoundException();
+           System.out.println();
           return mapper.mapping(all,ProfileDto.class);
        }
 
