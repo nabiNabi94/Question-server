17d7a8815cb1dfa7ce257ca7d96cd15e7fabb218
Vladislav Nabiev <v.nabi@icloud.com>
2022-03-11 17:54:14 +0300

    Add method: addProfile to the ProfileController

--- src/main/java/ru/digitalliague/questionsserver/controllers/ProfileController.java
+++ src/main/java/ru/digitalliague/questionsserver/controllers/ProfileController.java
@@ -2,9 +2,7 @@ package ru.digitalliague.questionsserver.controllers;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.digitalliague.questionsserver.modelDTO.ProfileDto;
 import ru.digitalliague.questionsserver.service.ProfileService;
 
@@ -18,8 +16,18 @@ public class ProfileController {
     private ProfileService profileService;
 
 
+    @GetMapping("/getAll")
     public ResponseEntity<List<ProfileDto>> getAllProfile(){
         List<ProfileDto> allProfile = profileService.getAllProfile();
         return ResponseEntity.ok().body(allProfile);
     }
+
+    @PostMapping("/add")
+    public ResponseEntity<String> addProfile(@RequestBody ProfileDto profileDto){
+        profileService.addProfile(profileDto);
+        return ResponseEntity.ok().body("Added Profile successfully");
+    }
+
+
+
 }
